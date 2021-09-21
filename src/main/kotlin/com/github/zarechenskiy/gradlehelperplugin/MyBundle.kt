08e2b7d9fbdb9ff659616d6db7ce5bd26537676b
GitHub Action <action@github.com>
2021-09-21 10:23:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/zarechenskiy/gradlehelperplugin/MyBundle.kt
+++ src/main/kotlin/com/github/zarechenskiy/gradlehelperplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.zarechenskiy.gradlehelperplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/zarechenskiy/gradlehelperplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.zarechenskiy.gradlehelperplugin.services
+
+import com.github.zarechenskiy.gradlehelperplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
